2f11f9a2d60463e7baca059a1778e8ce191fbc0a
Seth Schwartzman <sschwartzman@users.noreply.github.com>
2017-07-24 15:42:08 -0400

    Initial Working Commit!

---
+++ perfmon-infra-plugin/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace newrelic_infra_perfmon_plugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan pollingInterval = new TimeSpan(10000); // In ms
+            string configFile = "config.json";
+            if (args.Length > 0)
+            {
+                configFile = args[0];
+            }
+
+            StreamReader configFileReader = new StreamReader(configFile);
+            Config properties = JsonConvert.DeserializeObject<Config>(configFileReader.ReadToEnd());
+
+            string name = properties.name;
+            if (String.IsNullOrEmpty(name) || String.Equals(name, "ComputerName"))
+            {
+                name = Environment.MachineName;
+            }
+
+            List<Counterlist> counterlist = properties.counterlist;
+            if (counterlist.Count == 0)
+            {
+                throw new Exception("'counterlist' is empty. Do you have a 'config/plugin.json' file?");
+            }
+
+            PerfmonInfraService pService = new PerfmonInfraService(name, counterlist);
+            do
+            {
+                DateTime then = DateTime.Now;
+                pService.PollCycle();
+                DateTime now = DateTime.Now;
+                TimeSpan elapsedTime = now.Subtract(then);
+                if(pollingInterval.TotalMilliseconds > elapsedTime.TotalMilliseconds) {
+                    Thread.Sleep(pollingInterval - elapsedTime);
+                } else {
+                    Thread.Sleep(pollingInterval);
+                }
+            }
+            while(1 == 1);
+        }
+    }
+}
